ceaebb3c5da627160a0c4f9ed27fb452b258e63f
antronic <antronic.inc@gmail.com>
2016-09-16 03:46:50 +0700

    update lesson 4

--- public/04-Blur-Image-with-stackblur-js/canvas.js
+++ public/04-Blur-Image-with-stackblur-js/canvas.js
@@ -2,14 +2,20 @@ var canvas1 = document.getElementById('canvas1');
 var ctx1 = canvas1.getContext('2d');
 
 var image = new Image();
-document.getElementById('bg').naturalWidth = "300px";
-image.src = "/assets/ios_7_galaxy-wide.jpg";
+
+// image.src = "/assets/ios_7_galaxy-wide.jpg";
+image.src = "/assets/camp.jpg";
 
 image.addEventListener('load', loadImage);
 
 function loadImage(){
-    ctx1.drawImage(image, 0, 0, 300, 300);
-    StackBlur.canvasRGB(canvas1, 0, 0, 300, 300, 100);
+    var i_height = image.height / image.width * canvas1.height;
+    var i_width = image.width / image.height * canvas1.width;
+    var posX = (canvas1.width - i_width) / 2;
+
+    ctx1.drawImage(image, 0, 0, canvas1.width, canvas1.height);
+    StackBlur.canvasRGB(canvas1, 0, 0, canvas1.width, canvas1.height, 30);
+    ctx1.drawImage(image, posX, 0, i_width, canvas1.height );
     writeText();
 }
 
